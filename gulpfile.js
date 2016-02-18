07ef1535dbf15f219bf43da6bb2dc9c41284db2c
Omar Al-Ithawi <i@omardo.com>
2016-02-18 17:20:35 +0200

    Updates for better build, and updated mathjax from 2.5 to 2.6

--- gulpfile.js
+++ gulpfile.js
@@ -2,16 +2,16 @@ var gulp = require('gulp');
 var plumber = require('gulp-plumber');
 var uglify = require('gulp-uglify');
 var browserSync = require('browser-sync');
-var replace = require('gulp-replace');
 var jshint = require('gulp-jshint');
 var concat = require('gulp-concat');
+var order = require('gulp-order');
 
 
-gulp.task('browser-sync', function() {
+gulp.task('browser-sync', function () {
   browserSync({
     port: process.env.PORT || 3000,
     server: {
-       baseDir: '/code/'
+      baseDir: '/code/'
     }
   });
 });
@@ -22,22 +22,31 @@ gulp.task('bs-reload', function () {
 });
 
 
-gulp.task('scripts', function() {
+gulp.task('scripts', function () {
   return gulp.src('/code/extensions/arabic/unpacked/**/*.js')
     .pipe(plumber({
       errorHandler: function (error) {
         console.log(error.message);
         this.emit('end');
-    }}))
+      }
+    }))
+    .pipe(order([
+      'license.js',
+      '**/*.js'
+    ]))
     .pipe(concat('arabic.js'))
     .pipe(jshint())
-    .pipe(uglify())
+    .pipe(uglify({
+      preserveComments: 'some'
+    }))
     .pipe(gulp.dest('/code/extensions/arabic/'))
-    .pipe(browserSync.reload({stream: true}))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
 });
 
 
-gulp.task('default', ['scripts', 'browser-sync'], function() {
-  gulp.watch('/code/extensions/arabic/unpacked/*.js', ['scripts']);
+gulp.task('default', ['scripts', 'browser-sync'], function () {
+  gulp.watch('/code/extensions/arabic/unpacked/**/*.js', ['scripts']);
   gulp.watch('/code/testcases/**/*.{html,css,js}', ['bs-reload']);
 });
